f6ac6e2e61cc79ecf74a9ef496e25e892e76cfe9
ewiener <ewiener@yahoo.com>
2016-12-22 22:44:58 -0800

    Updates from codegen

--- lib/pace_funding/api/post_application_api.rb
+++ lib/pace_funding/api/post_application_api.rb
@@ -38,8 +38,8 @@ module PaceFunding
     # @param salesperson_code Salesperson Code
     # @param project_id Project Id
     # @param notify_url PACE Funding send the credit decision on notify url
-    # @param property_type Property detail
-    # @param property_ownership Property detail
+    # @param property_type Property type
+    # @param property_ownership Property ownership
     # @param fo_first_name First Property Owner
     # @param fo_last_name First Property Owner
     # @param fo_ssn (123-45-6789)First Property Owner
@@ -47,9 +47,9 @@ module PaceFunding
     # @param fo_email First Property Owner
     # @param fo_phone First Property Owner
     # @param [Hash] opts the optional parameters
-    # @option opts [String] :property_ownership_name Property detail. if Property Ownership is Trust,Corporation or LLC ,Other
+    # @option opts [String] :property_ownership_name Property ownership name. If Property Ownership is Trust,Corporation or LLC,Other
     # @option opts [String] :fo_suffix First Property Owner
-    # @option opts [Integer] :fo_same_as Same As Property Address-&gt; First Property Owner
+    # @option opts [String] :fo_same_as Same As Property Address-&gt; First Property Owner
     # @option opts [String] :fo_address First Property Owner Mailing Address
     # @option opts [String] :fo_state First Property Owner Mailing Address
     # @option opts [String] :fo_city First Property Owner Mailing Address
@@ -62,16 +62,16 @@ module PaceFunding
     # @option opts [String] :so_dob mm/dd/yyyy Second Property Owner
     # @option opts [String] :so_email Second Property Owner
     # @option opts [String] :so_phone Second Property Owner
-    # @option opts [Integer] :so_same_as Same As Property Address-&gt; First Property Owner
+    # @option opts [String] :so_same_as Same As Property Address-&gt; Second Property Owner
     # @option opts [String] :so_address Second Property Owner Mailing Address
     # @option opts [String] :so_state Second Property Owner Mailing Address
     # @option opts [String] :so_city Second Property Owner Mailing Address
     # @option opts [String] :so_zipcode Second Property Owner Mailing Address
     # @option opts [String] :so_unit Second Property Owner Mailing Address
-    # @return [nil]
+    # @return [PostApplicationResponse]
     def post_application_post(auth_key, contractor_code, salesperson_code, project_id, notify_url, property_type, property_ownership, fo_first_name, fo_last_name, fo_ssn, fo_dob, fo_email, fo_phone, opts = {})
-      post_application_post_with_http_info(auth_key, contractor_code, salesperson_code, project_id, notify_url, property_type, property_ownership, fo_first_name, fo_last_name, fo_ssn, fo_dob, fo_email, fo_phone, opts)
-      return nil
+      data, _status_code, _headers = post_application_post_with_http_info(auth_key, contractor_code, salesperson_code, project_id, notify_url, property_type, property_ownership, fo_first_name, fo_last_name, fo_ssn, fo_dob, fo_email, fo_phone, opts)
+      return data
     end
 
     # Filled In Credit App
@@ -81,8 +81,8 @@ module PaceFunding
     # @param salesperson_code Salesperson Code
     # @param project_id Project Id
     # @param notify_url PACE Funding send the credit decision on notify url
-    # @param property_type Property detail
-    # @param property_ownership Property detail
+    # @param property_type Property type
+    # @param property_ownership Property ownership
     # @param fo_first_name First Property Owner
     # @param fo_last_name First Property Owner
     # @param fo_ssn (123-45-6789)First Property Owner
@@ -90,9 +90,9 @@ module PaceFunding
     # @param fo_email First Property Owner
     # @param fo_phone First Property Owner
     # @param [Hash] opts the optional parameters
-    # @option opts [String] :property_ownership_name Property detail. if Property Ownership is Trust,Corporation or LLC ,Other
+    # @option opts [String] :property_ownership_name Property ownership name. If Property Ownership is Trust,Corporation or LLC,Other
     # @option opts [String] :fo_suffix First Property Owner
-    # @option opts [Integer] :fo_same_as Same As Property Address-&gt; First Property Owner
+    # @option opts [String] :fo_same_as Same As Property Address-&gt; First Property Owner
     # @option opts [String] :fo_address First Property Owner Mailing Address
     # @option opts [String] :fo_state First Property Owner Mailing Address
     # @option opts [String] :fo_city First Property Owner Mailing Address
@@ -105,13 +105,13 @@ module PaceFunding
     # @option opts [String] :so_dob mm/dd/yyyy Second Property Owner
     # @option opts [String] :so_email Second Property Owner
     # @option opts [String] :so_phone Second Property Owner
-    # @option opts [Integer] :so_same_as Same As Property Address-&gt; First Property Owner
+    # @option opts [String] :so_same_as Same As Property Address-&gt; Second Property Owner
     # @option opts [String] :so_address Second Property Owner Mailing Address
     # @option opts [String] :so_state Second Property Owner Mailing Address
     # @option opts [String] :so_city Second Property Owner Mailing Address
     # @option opts [String] :so_zipcode Second Property Owner Mailing Address
     # @option opts [String] :so_unit Second Property Owner Mailing Address
-    # @return [Array<(nil, Fixnum, Hash)>] nil, response status code and response headers
+    # @return [Array<(PostApplicationResponse, Fixnum, Hash)>] PostApplicationResponse data, response status code and response headers
     def post_application_post_with_http_info(auth_key, contractor_code, salesperson_code, project_id, notify_url, property_type, property_ownership, fo_first_name, fo_last_name, fo_ssn, fo_dob, fo_email, fo_phone, opts = {})
       if @api_client.config.debugging
         @api_client.config.logger.debug "Calling API: PostApplicationApi.post_application_post ..."
@@ -128,8 +128,16 @@ module PaceFunding
       fail ArgumentError, "Missing the required parameter 'notify_url' when calling PostApplicationApi.post_application_post" if notify_url.nil?
       # verify the required parameter 'property_type' is set
       fail ArgumentError, "Missing the required parameter 'property_type' when calling PostApplicationApi.post_application_post" if property_type.nil?
+      # verify enum value
+      unless ['Single-Family', 'Condo/Townhome', 'Mobile Home', 'Multi-Family'].include?(property_type)
+        fail ArgumentError, "invalid value for 'property_type', must be one of Single-Family, Condo/Townhome, Mobile Home, Multi-Family"
+      end
       # verify the required parameter 'property_ownership' is set
       fail ArgumentError, "Missing the required parameter 'property_ownership' when calling PostApplicationApi.post_application_post" if property_ownership.nil?
+      # verify enum value
+      unless ['Individual', 'Joint', 'Trust', 'Corporation or LLC', 'Other'].include?(property_ownership)
+        fail ArgumentError, "invalid value for 'property_ownership', must be one of Individual, Joint, Trust, Corporation or LLC, Other"
+      end
       # verify the required parameter 'fo_first_name' is set
       fail ArgumentError, "Missing the required parameter 'fo_first_name' when calling PostApplicationApi.post_application_post" if fo_first_name.nil?
       # verify the required parameter 'fo_last_name' is set
@@ -142,6 +150,12 @@ module PaceFunding
       fail ArgumentError, "Missing the required parameter 'fo_email' when calling PostApplicationApi.post_application_post" if fo_email.nil?
       # verify the required parameter 'fo_phone' is set
       fail ArgumentError, "Missing the required parameter 'fo_phone' when calling PostApplicationApi.post_application_post" if fo_phone.nil?
+      if opts[:'fo_same_as'] && !['1', '0'].include?(opts[:'fo_same_as'])
+        fail ArgumentError, 'invalid value for "fo_same_as", must be one of 1, 0'
+      end
+      if opts[:'so_same_as'] && !['1', '0'].include?(opts[:'so_same_as'])
+        fail ArgumentError, 'invalid value for "so_same_as", must be one of 1, 0'
+      end
       # resource path
       local_var_path = "/post-application".sub('{format}','json')
 
@@ -204,7 +218,8 @@ module PaceFunding
         :query_params => query_params,
         :form_params => form_params,
         :body => post_body,
-        :auth_names => auth_names)
+        :auth_names => auth_names,
+        :return_type => 'PostApplicationResponse')
       if @api_client.config.debugging
         @api_client.config.logger.debug "API called: PostApplicationApi#post_application_post\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
       end
